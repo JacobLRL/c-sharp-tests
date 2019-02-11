c4e25e7b67c6c4ef178d3b8aceda9a6ce4a3dc0c
JacobLRL <JLittle@spartaglobal.com>
2019-02-11 15:26:43 +0000

    Add project files.

---
+++ SomeTests/SomeTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeTests
+{
+    public class SomeTest
+    {
+        // takes a string change any text into lowercase then uppercase on repeat, ignoring spaces
+        public string TextToSpongeBobMeme(string str) {
+            return "";
+        }
+
+        // takes 3 words, return a string removing the middle word leaving one space
+        public string RemoveMiddleWord(string str) {
+            return "";
+        }
+
+        // takes an array of ints, return the sum of the array NOT INCLUDING THE LARGEST NUMBER
+        public int SumIgnoringMax(int[] intArr) {
+            return -1;
+        }
+
+        // takes 2 ints which decides how high the pyramid goes
+        /*example for 5, 9
+
+             *
+            ***
+           *****
+          *******
+         *********
+     
+        */
+        public char[,] Pyramid(int height, int width) {
+            char[,] pyramid = new char[height, width];
+            //some code to make it work, good luck 
+            
+            return pyramid;
+        }
+    }
+}
